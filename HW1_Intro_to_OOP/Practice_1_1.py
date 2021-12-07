f0ef68e0d41c78bfbbaec5d85de7b98c4db43469
naglenko.ig <naglenko.ig@gmail.com>
2021-12-07 07:53:20 +0200

    add practice 1

--- HW1_Intro_to_OOP/Practice_1_1.py
+++ HW1_Intro_to_OOP/Practice_1_1.py
@@ -1,1 +1,16 @@
-11111222
+class RectAng:
+    def __init__(self, side_a, side_b):
+        self.side_a = side_a
+        self.side_b = side_b
+
+    def __str__(self):
+        return "Rectangle [side_a = {}, side_b = {}]".format(self.side_a, self.side_b)
+
+    def get_square(self):
+        return self.side_a * self.side_b
+
+
+my_figure = RectAng(3, 4)
+print(my_figure.get_square())
+
+print(my_figure)
