00868e91e28910e7cc029dedfe35135e3c341e21
naglenko.ig <naglenko.ig@gmail.com>
2021-12-27 09:58:18 +0200

    home_work 6_1 add

---
+++ HW_6_Iterators/HW_6_1_Group/Group.py
@@ -0,0 +1,77 @@
+# Додана обробка вийнятків для кількості студентів
+class GroupOverLimit(Exception):
+    def __init__(self, student):
+        super().__init__()
+        self.student = student
+
+    def __str__(self):
+        return "Group is over limited. {} could not be added".format(self.student)
+
+
+# Клас Група
+class Group:
+    def __init__(self, group_name, start_year, grad_year):
+        self.group_name = group_name
+        self.start_year = start_year
+        self.grad_year = grad_year
+        self.list_students = []
+
+    def __str__(self):
+        txt = "Group: {}, {} - {} \n".format(self.group_name, self.start_year, self.grad_year)
+        if len(self.list_students) == 0:
+            txt += "No list_students in this group"
+        else:
+            i = 1
+            for stud in self.list_students:
+                txt += str(i) + ". " + str(stud) + "\n"
+                i += 1
+        return txt
+
+    def add_stud(self, student):
+        try:
+            if len(self.list_students) >= 10:
+                raise GroupOverLimit(student)  # Виключення "переліміт групи"
+            else:
+                self.list_students.append(student)
+                print("Student added successfully: {} {}".format(student.name, student.surname))
+        except GroupOverLimit as err:
+            print(err)
+
+    def del_stud(self, surname, name):
+        for student in self.list_students:
+            if surname == student.surname and name == student.name:
+                print("Deleted " + str(student))
+                self.list_students.remove(student)
+        return "Student deleted successfully"
+
+    def search_by_surname(self, surname):
+        found_item = None
+        for student in self.list_students:
+            if surname == student.surname:
+                found_item = student
+        return found_item
+
+    def __len__(self):
+        return len(self.list_students)
+
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            if index >=0 and index < len(self.list_students):
+                return self.list_students[index]
+            else:
+                raise IndexError
+        if isinstance(index, slice):
+            start = 0 if index.start is None else index.start
+            stop = len(self.list_students) - 1 if index.stop is None else index.stop
+            step = 1 if index.step is None else index.step
+            if start < 0 and stop >= len(self.list_students):
+                raise IndexError
+            else:
+                result = []
+                for i in range(start, stop, step):
+                    result.append(self.list_students[i])
+                return result
+        raise TypeError()
+
+
+
---
+++ HW_6_Iterators/HW_6_1_Group/main.py
@@ -0,0 +1,42 @@
+from HW_6_Iterators.HW_6_1_Group.Group import Group
+from HW_6_Iterators.HW_6_1_Group.Student import Student
+
+if __name__ == "__main__":
+    # Створення екземлярів класів
+
+    # Студент
+    student_1 = Student("Jack", "Johnson", 18, "M", "KPI", "FIOT")
+    student_2 = Student("Nicole", "Woodworth", 19, "F", "KPI", "FIOT")
+    student_3 = Student("stud3", "test", 20, "M", "KPI", "IHF")
+    student_4 = Student("stud4", "test", 20, "M", "KPI", "IHF")
+    student_5 = Student("stud5", "test", 20, "M", "KPI", "IHF")
+    student_6 = Student("stud6", "test", 20, "M", "KPI", "IHF")
+    student_7 = Student("stud7", "test", 20, "M", "KPI", "IHF")
+    student_8 = Student("stud8", "test", 20, "M", "KPI", "IHF")
+    student_9 = Student("stud9", "test", 20, "M", "KPI", "IHF")
+    student_10 = Student("stud10", "test", 20, "M", "KPI", "IHF")
+    student_11 = Student("stud11", "test", 20, "M", "KPI", "IHF")
+
+    # Створення групи
+    my_group = Group("FIOT", 2010, 2015)
+
+    # Додаємо студентів у групу
+    my_group.add_stud(student_1)
+    my_group.add_stud(student_2)
+    my_group.add_stud(student_3)
+    my_group.add_stud(student_4)
+    my_group.add_stud(student_5)
+    my_group.add_stud(student_6)
+    my_group.add_stud(student_7)
+    my_group.add_stud(student_8)
+    my_group.add_stud(student_9)
+    my_group.add_stud(student_10)
+    # my_group.add_stud(student_11)
+
+
+    print(len(my_group))
+    print(my_group[5])
+
+    test_group = (my_group[3:])
+    for t in test_group:
+        print(t)
