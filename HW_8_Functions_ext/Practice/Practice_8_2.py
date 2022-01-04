0278c736b704418c35d89279bd40b7c284acf344
naglenko.ig <naglenko.ig@gmail.com>
2022-01-04 20:25:55 +0200

    add home_work 8_2

--- HW_8_Functions_ext/Practice/Practice_8_2.py
+++ HW_8_Functions_ext/Practice/Practice_8_2.py
@@ -1,49 +1,43 @@
+# 2) Используя функцию замыкания реализуйте такой прием программирования
+# как Мемоизация - https://en.wikipedia.org/wiki/Memoization
+# Используйте  полученный  механизм  для  ускорения  функции  рекурсивного
+# вычисления n — го члена ряда Фибоначчи. Сравните скорость выполнения с
+# просто рекурсивным подходом.
+
 import time
 
-print()
 
-
-def fibonacci_recursive(value):  # рекурсивная функция
+def f_by_recursion(value):
     if (value == 1) or (value == 2):
         return 1
     elif value > 2:
-        return fibonacci_recursive(value - 1) + fibonacci_recursive(value - 2)
+        return f_by_recursion(value - 1) + f_by_recursion(value - 2)
 
 
-start = time.time()
-
+start_at = time.time_ns()
 for i in range(35):
-    fibo_rec = fibonacci_recursive(i)
-    print(fibo_rec)
-print('Time is: ', time.time() - start)
-
-fibonacci_cache = {}
+    print(i, '->', f_by_recursion(i))
+end_at = time.time_ns()
+print('Elapsed time: ', (end_at - start_at), "nsec", i, "members")
 
 
-def fibonacci_memo():  # обемлюющая функция
-    x1, x2 = 0, 1
-    cach = {}
+def fib_memoization():
+    mem_cache = {0: 0, 1: 1}
 
-    def next_value(n):  # замыкание
-        nonlocal x1, x2
-        if n in cach: return cach[n]
-        if n == 0:
-            cach[n] = 0
-        elif n == 1:
-            cach[n] = 1
+    def fib_member(n):
+        if n in mem_cache:
+            return mem_cache[n]
         else:
-            x3 = x1 + x2
-            x1, x2 = x2, x3
-            cach[n] = x3
-        return cach[n]
+            mem_cache[n] = fib_member(n - 1) + fib_member(n - 2)
+            return mem_cache[n]
 
-    return next_value
+    return fib_member
 
 
-start = time.time()
-
-fibonacci = fibonacci_memo()  # Вызов объемлющей функции
-for i in range(2, 5000):
-    my_fibo = fibonacci(i)  # Вызов вложенной функции (замыкания)
-    print(my_fibo)
-print('Time is: ', time.time() - start)
+start_at = time.time_ns()
+memoization_test = fib_memoization()
+for i in range(35):
+    f_member = memoization_test(i)
+    print(i, '->', f_member)
+end_at = time.time_ns()
+print('Elapsed time: ', (end_at - start_at), "nsec", i, "members")
