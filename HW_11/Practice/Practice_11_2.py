e41429eb257ebacf909dd26981169fdb0a6d43a8
naglenko.ig <naglenko.ig@gmail.com>
2022-01-17 00:20:30 +0200

    add practice 11_2

---
+++ HW_11/Practice/Practice_11_2.py
@@ -0,0 +1,25 @@
+# var 1
+from math import sqrt
+# class Triangle:
+#     def __init__(self, a, b, c):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+#
+#     def __str__(self):
+#         return "Triangle [a = {}, b = {}, c={}, area={}]".format(self.a, self.b, self.c, self.area)
+#
+#     def get_area(self):
+#         print("This is virtual field")
+#         p = (self.a + self.b + self.c) / 2.00
+#         area = sqrt((p * (p - self.a) * (p - self.b) * (p - self.c)))
+#         return area
+#
+#     area = property(get_area)
+#
+#
+# figure1 = Triangle(3, 4, 5)
+#
+# print(figure1.area)
+# figure1.area = 12
+
