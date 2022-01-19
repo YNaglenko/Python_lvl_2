491c76beed7b0a61abc5cd8c72c65c90c5a0f66e
naglenko.ig <naglenko.ig@gmail.com>
2022-01-19 22:59:23 +0200

    add practice 11_3

---
+++ HW_11/Home_work/Home_work_11_1.py
@@ -0,0 +1,28 @@
+# 1) Создайте дескриптор, который будет делать поля класса управляемые им
+# доступными только для чтения.
+
+class ReadOnlyDescriptor:
+    def __init__(self, value):
+        self.value = value
+
+    def __get__(self, instance_self, instance_class):
+        return self.value
+
+    def __delete__(self, instance_self):
+        raise AttributeError("fields in read only mode")
+
+    def __set__(self, instance_self, value):
+        raise AttributeError("fields in read only mode")
+
+
+class Card:
+    card_mask = ReadOnlyDescriptor("404259***37")
+
+    def __str__(self):
+        return "Card [ Number: {}]".format(self.card_mask)
+
+
+newCard = Card()
+print(newCard)
+
+newCard.card_mask = 111
